26cb463a30d0b1c33e55670f79fd44edd69a82f8
GrgSumin <be2019se673@gces.edu.np>
2024-07-26 08:59:15 +0545

    Create button component and tailwind config

---
+++ app/components/button.tsx
@@ -0,0 +1,25 @@
+import { cn } from "../../utils/cn";
+import { Text, TouchableOpacity } from "react-native";
+
+interface ButtonProps {
+  label: string;
+  variant: "primary" | "transparent" | "white";
+  textColor: string;
+}
+
+const Button = ({ label, variant, textColor }: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      className={cn(
+        "flex items-center justify-center  rounded-md cursor-pointer transition-colors h-11 w-80",
+        variant === "primary" && "bg-[#212121] ",
+        variant === "white" && "bg-white border-[1px] border-[#212121] ",
+        variant === "transparent" && "bg-transparent border-[1px] border-white "
+      )}
+    >
+      <Text className={cn("font-light text-lg", textColor)}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
